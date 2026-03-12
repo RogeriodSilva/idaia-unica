8723d388711fa23f7579c0c145cd437a8c26252b
RogeriodSilva <ro.juniorperso@gmail.com>
2026-03-12 19:14:49 -0300

    api response sub

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,13 @@
-function tempo(request, response){
+async function tempo(request, response){
      const dynamicDate = new Date();
 
+     const subsResponse = await fetch("https://api.convertkit.com/v3/subscribers?api_secret=ZJwL0hi01EvB90CIpsyw1e0fGRzPfw1Q");
+     const subsResponseJSON = await subsResponse.json();
+     const inscritos = subsResponseJSON.total_subscribers;
+
      response.json({
-          date: dynamicDate.toGMTString()
+          date: dynamicDate.toGMTString(),
+          subs: inscritos
      });
 }
 
