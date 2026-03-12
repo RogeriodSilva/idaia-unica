ff3bf91e10c1b468cfa02f511e6a0192b939533d
RogeriodSilva <ro.juniorperso@gmail.com>
2026-03-12 08:39:38 -0300

    Nova Home

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,11 @@
-import { useState } from "react"
 
 function Home() {
      
      return( 
      <>
           <h1>Home</h1>
-          <Contador/>
      </>)
 }
 
-function Contador(){
-     const [contador, setContador] = useState(0)
-     
-     return <>
-          <div>
-               <div>{contador}</div>
-               <button onClick={()=> setContador(contador + 1)}>Clique aqui</button>
-          </div>
-     </>
-}
 
 export default Home
